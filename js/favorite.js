7a02d6a2139f4b0fcdf3f2e9d6cd911bf63f9d07
João Vitor Gruppo <32822736+joaogpp@users.noreply.github.com>
2023-05-20 18:42:53 -0300

    Add files via upload

--- js/favorite.js
+++ js/favorite.js
@@ -1,24 +1,107 @@
+import { GithubUser } from "./githubUsers.js"
+
 export class Favorites {
   constructor(root){
     this.root = document.querySelector(root)
-    this.tbody = document.querySelector('table tbody')
+    this.load()
+  }
+
+  load() {
+    this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+  }
+
+  save() {
+    localStorage.setItem(`@github-favorites:`, JSON.stringify(this.entries))
+  }
+
+  delete(username){ 
+    const filteredEntries = this.entries.filter(entry => entry.login !== username.login)
+    this.entries = filteredEntries
+    this.update()
+    this.save()
+  } 
+
+  async add(username) {
+    
+    try {
+      const userExists = this.entries.find(entry => entry.login.toLowerCase() == username.toLowerCase())
+      if(userExists) {
+        throw new Error(`Usuario ja cadastrado`)
+      } 
+      const user = await GithubUser.search(username)
+      
+      if(user.login === undefined) {
+        throw new Error(`Usuario nao encontrado`)
+      }
+
+      this.entries = [user, ...this.entries]
+      this.update()
+      this.save()
+
+    } catch (error) {
+      alert(error.message)
+    }
+
   }
 }
 
 export class FavoritesView extends Favorites {
   constructor(root) {
     super(root)
-
+    
+    this.tbody = document.querySelector('table tbody')
     this.update()
+    this.onAdd()
   }
 
+  onAdd() {
+    const button = this.root.querySelector('.search button')
+    button.addEventListener('click', () => {
+      const { value } = this.root.querySelector('.search input')
+      
+      this.add(value)
+    })
+  }
+  
   update() {
+      this.removeAllTr()
 
-    this.removeAllTr()
+      if(this.entries.length == 0) {
+      const tr = document.createElement(`tr`)
+      tr.classList.add('first')
+      tr.innerHTML = `
+      <td colspan="4">
+            <div class="empty">
+              <img src="./images/bg-star.svg" alt="">
+              <p>Nenhum favorito ainda</p>
+            </div>
+          </td>
+      `
+      this.tbody.append(tr)
 
-    this.tbody.append(this.createRow())
-    this.tbody.append(this.createRow())
-    this.tbody.append(this.createRow())
+    }
+
+    this.entries.forEach((user) => {
+      const row = this.createRow()
+
+      row.querySelector(`.user img`).src = `https://github.com/${user.login}.png`
+      row.querySelector(`.user a`).href = `https://github.com/${user.login}`
+      row.querySelector(`.user a p`).textContent = user.name
+      row.querySelector(`.user a span`).textContent = user.login
+      row.querySelector(`.repositories`).textContent = user.public_repos
+      row.querySelector(`.followers`).textContent = user.followers
+
+      row.querySelector(`.remove`).addEventListener('click', () => {
+        const isOk = confirm(`Deseja realmente remover essa linha?`)
+
+        if(isOk) {
+          this.delete(user)
+        }
+      })
+
+      this.tbody.append(row)
+    
+    })
   }
 
   createRow() {
@@ -47,8 +130,8 @@ export class FavoritesView extends Favorites {
 
   removeAllTr() {
     const trs = this.tbody.querySelectorAll('tr')
-    trs.forEach((user) => {
-      user.remove()
+    trs.forEach((tr) => {
+      tr.remove()
     })
   }
 }
